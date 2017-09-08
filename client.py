3624bde2a537b9125397b3d247109b233a31bef1
Koray <korayyaksoy@gmail.com>
2017-09-08 23:15:47 +0300

    client.py added

---
+++ client.py
@@ -0,0 +1,18 @@
+import socket
+
+def make_request():
+    url  = input("Please enter the url: ")
+    
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    ip   = socket.gethostbyname(url)
+    port = 80
+    sock.connect((ip,port))
+    
+    sock.send(("GET / HTTP/1.1\nHost: {}\n\n".format(url)).encode())
+    response = sock.recv(2048).decode()
+    sock.close()
+    
+    return response
+    
+
+print(make_request())
